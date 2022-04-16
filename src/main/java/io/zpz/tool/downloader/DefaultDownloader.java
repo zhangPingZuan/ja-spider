284650fcb262f4e6f6bba2c777042621ed36ab75
zhangpingzuan <pzzhang@91souban.com>
2022-04-16 21:24:31 +0800

    feat(spider): add Spider Downloader Scheduler CentralEngine etc interface and init Downloader

--- src/main/java/io/zpz/tool/downloader/DefaultDownloader.java
+++ src/main/java/io/zpz/tool/downloader/DefaultDownloader.java
@@ -2,7 +2,6 @@ package io.zpz.tool.downloader;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
