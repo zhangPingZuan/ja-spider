057892f04c0fb553d8a002f39861e966de9bd66b
zhangpingzuan <pzzhang@91souban.com>
2022-04-22 13:39:50 +0800

    feat(spider): imp CrawlingResponse

--- src/main/java/io/zpz/tool/MainSpider.java
+++ src/main/java/io/zpz/tool/MainSpider.java
@@ -5,6 +5,7 @@ import io.zpz.tool.engine.CentralEngine;
 import io.zpz.tool.engine.DefaultCentralEngine;
 import io.zpz.tool.engine.SimpleEngineEventMulticaster;
 import io.zpz.tool.spider.DownLoadedEngineEventSpider;
+import io.zpz.tool.spider.FreeReadSpiderItem;
 import io.zpz.tool.spider.Spider;
 import io.zpz.tool.task.DefaultTaskManager;
 import io.zpz.tool.task.TaskManager;
@@ -16,8 +17,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-
 @Setter
 @Getter
 @Slf4j
@@ -26,6 +25,8 @@ public class MainSpider {
     public static void main(String[] args) {
 
         SpringApplication.run(MainSpider.class, args);
+
+        // 配置引擎
         TaskManager taskManager = new DefaultTaskManager();
         CentralEngine centralEngine = DefaultCentralEngine.builder()
                 .downloader(new DefaultDownloader())
@@ -33,14 +34,29 @@ public class MainSpider {
                 .taskManager(taskManager)
                 .scheduler(null)
                 .build();
-        // 注册 spider
-        Spider<?> spider = DownLoadedEngineEventSpider.builder().build();
-        centralEngine.getEngineEventMulticaster().addEngineEventListener(spider);
-        centralEngine.start();
 
+        // 配置spider
         FinalProcessor finalProcessor = new MysqlFinalProcessor();
-        finalProcessor.process(new ArrayList<>());
+        Spider<?> spider = DownLoadedEngineEventSpider.builder()
+                .taskManager(taskManager)
+                .finalProcessor(finalProcessor)
+                .build();
+        spider.addSpiderItem(FreeReadSpiderItem.builder()
+                .regex("https://www.shuquge.com/")
+                .build());
 
+        // 注册spider
+        centralEngine.getEngineEventMulticaster().addEngineEventListener(spider);
+
+        // 启动引擎
+        centralEngine.start();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // 关闭引擎
         centralEngine.stop();
     }
 
